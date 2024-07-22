bcbb98ec120d335df31cef34d81a27d858e05d75
rishabhchaudhary2 <rishabhnm786@gmailcom>
2024-07-22 20:33:51 +0530

    updated

--- day4.js
+++ day4.js
@@ -76,6 +76,24 @@ for (let i = 0; i < 5; i++) {
     
 }
 
+// Activity 5 
+// PRINT 1 TO 10 BUT SKIP 5
+    for (let i = 1; i <=10; i++) {
+        if(i==5)
+            continue;
+        console.log(i);;
+        
+    }
+
+
+// PRINT FROM 1 TO 10 BUT STOP AT 7
+for (let i = 1; i <=10; i++) {
+    if(i==7)
+        break;
+    console.log(i);;
+    
+}
+ 
 // outputs :
 
 // 1
@@ -131,3 +149,68 @@ for (let i = 0; i < 5; i++) {
 // ***
 // ****
 // *****
+
+// TASK 8;
+/*1
+2
+3
+4
+6
+7
+8
+9
+10
+// TASK 9
+1
+2
+3
+4
+5
+6
+*/
+
+// FEATURE REQUEST
+// Number Printing Script
+
+for (let i = 1; i <=10; i++) {
+
+    console.log(i);;
+    
+}
+i=1
+while (i < 11) {
+    console.log(i);;
+    i++;
+}
+
+
+
+// JavaScript script to print the multiplication table of 5
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`5 x ${i} = ${5 * i}`);
+  }
+
+//Summation printing script:
+let ij = 1;
+let sum3 = 0;
+while (ij < 11) {
+    sum += ij;
+    ij++;
+}
+console.log(`Sum from 1 to 10 is ${sum3}`);
+
+// Factorial calculating script
+
+let number1=prompt("enter the number to calculate factorial")
+
+do {
+    if (number1< 0) {
+        console.log("1");
+        break;
+    }
+    ans *= k;
+    k--
+
+} while (k > 0);
+console.log(` the factorial of ${number1} is ${ans}`);
